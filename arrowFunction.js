6039d06b496ec6f9f6c0987d0b14ef70869c7d53
Daniiielee <161543906+Daniiielee@users.noreply.github.com>
2026-02-04 20:53:36 -0500

    Desarrollo actividad 1, funcion flecha y regular

---
+++ arrowFunction.js
@@ -0,0 +1,4 @@
+const numeroArrow = (a) => {if (a%2===0){return "El numero es par"}
+    else{return "El numero es impar"}}
+
+console.log(numero(5));
---
+++ normalFunction.js
@@ -0,0 +1,9 @@
+function numero(a){
+    if (a%2===0){
+        return "El numero es par"
+    }else{
+        return "El numero es impar"
+    }
+}
+
+console.log(numero(5));
